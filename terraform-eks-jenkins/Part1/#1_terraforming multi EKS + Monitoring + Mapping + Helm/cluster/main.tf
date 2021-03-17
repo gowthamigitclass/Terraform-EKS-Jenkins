5aa43d792814d502b9a11b86fea4af67fd61bb20
gowrepo <35074202+gowthamigitclass@users.noreply.github.com>
2021-03-17 16:18:06 +0100

    Update main.tf

--- terraform-eks-jenkins/Part1/#1_terraforming multi EKS + Monitoring + Mapping + Helm/cluster/main.tf
+++ terraform-eks-jenkins/Part1/#1_terraforming multi EKS + Monitoring + Mapping + Helm/cluster/main.tf
@@ -17,6 +17,12 @@ variable "cluster_name" {
 variable "instance_type" {
   default = "m5.large"
 }
+  
+variable "iam_role_to_rbac_group_mappings" {
+  description = "Mapping of AWS IAM roles to RBAC groups, where the keys are the AWS ARN of IAM roles and the values are the mapped k8s RBAC group names as a list."
+  type        = map(list(string))
+  default     = {}
+}
 
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
